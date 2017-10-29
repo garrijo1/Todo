c3c4dc2636d93027144d84c86dfffc14763cb79c
Joseph Garrison <braisengeek@gmail.com>
2017-10-28 19:36:28 -0500

    commit

--- exam/index.js
+++ exam/index.js
@@ -1,15 +1,11 @@
-var express = require('express');
-var config = require('./config/config');
-var logger = require('./config/logger');
-var app = express();
+var express = require('express'),
+config = require('./config/config'); 
 
-//var port = process.env.port || 3000
+var app = express();    
 
 require('./config/express')(app, config);
-logger.log("Creating HTTP server on port: "+config.port);
+console.log("Creating HTTP server on port: " + config.port);
 
 require('http').createServer(app).listen(config.port, function () {
-    logger.log("HTTP Server listening on port: "+config.port+", in " +app.get('env')+" mode");
+console.log("HTTP Server listening on port: " + config.port + ", in " + app.get('env') + " mode");
 });
-
-module.exports=app;
